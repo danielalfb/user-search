1532aafecf3a9703ecdf7711b5a2aba42331b0b4
danielalfb <danielalfbarbosa@gmail.com>
2020-05-26 17:39:38 -0300

    preloader

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-let userSubmit = null;
-let userInput = null;
+let globaluserSubmit = null;
+let globaluserInput = null;
 
 let userFilter = null;
+let preLoader = null;
 
 let usersResult = null;
 let statsResult = null;
@@ -15,57 +16,58 @@ let totalAverageAge = 0;
 let userArray = [];
 let userArrayResult = [];
 
-
 window.addEventListener('load', () => {
-
-  userSubmit = document.querySelector('#userSubmit');
-  userInput = document.querySelector('#userInput');
+  globaluserSubmit = document.querySelector('#userSubmit');
+  globaluserInput = document.querySelector('#userInput');
   usersResult = document.querySelector('#usersResult');
   statsResult = document.querySelector('#statsResult');
 
   numberFormat = Intl.NumberFormat('pt-BR');
 
   fetchUsers();
+  hideLoader();
 });
 
 async function fetchUsers() {
-  const res = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo');
+  const res = await fetch(
+    'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo'
+  );
   const json = await res.json();
-  userArray = json.results.map(user => {
-    const {
-      gender,
-      name,
-      dob,
-      picture
-    } = user;
+  userArray = json.results.map((user) => {
+    const { gender, name, dob, picture } = user;
     return {
       gender,
-      name: name.first + " " + name.last,
+      name: name.first + ' ' + name.last,
       age: dob.age,
-      picture: picture.thumbnail
-    }
+      picture: picture.thumbnail,
+    };
   });
 
   activateButton();
 }
 
 function activateButton() {
-  userInput.addEventListener('input', () => {
-    if (userInput.value != '') {
-      userSubmit.disabled = false;
+  globaluserInput.addEventListener('input', () => {
+    if (globaluserInput.value != '') {
+      globaluserSubmit.disabled = false;
     }
   });
   inputClick();
 }
 
 function inputClick() {
-  userSubmit.addEventListener('click', () => {
-    userFilter = userArray.filter((user) =>
-      user.name.toLowerCase().indexOf(userInput.value.toLowerCase()) != -1
-    ).sort((a, b) => {
-      return a.name.localeCompare(b.name);
-    });
-  })
+  globaluserSubmit.addEventListener('click', () => {
+    userFilter = userArray
+      .filter(
+        (user) =>
+          user.name
+            .toLowerCase()
+            .indexOf(globaluserInput.value.toLowerCase()) != -1
+      )
+      .sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+  });
   render();
 }
 
@@ -73,5 +75,12 @@ function render() {
   const userHTML = `
   <div id="userContainer">
   <h4>${userFilter.length} usuário(s) encontrado(s)</h5>
-  `
+  `;
+}
+
+function hideLoader() {
+  preLoader = document.querySelector('#loading');
+  setTimeout(function () {
+    preLoader.classList.add('hide');
+  }, 2000);
 }
